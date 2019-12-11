86dbdc96f4380d64b18bf2dc7c30987450fda7bd
Adam <adamjackson@pursuit.org>
2019-12-11 12:15:27 -0500

    Added Identifiers

---
+++ Vybz/StoryboardIDs/VCIds.swift
@@ -0,0 +1,17 @@
+//
+//  VCIds.swift
+//  Vybz
+//
+//  Created by God on 12/10/19.
+//  Copyright © 2019 God. All rights reserved.
+//
+
+import Foundation
+enum VCIds: String {
+    //TABS
+    case MoodVC
+    case PianoTabVC
+    case UserProfileVC
+    case EducationVC
+    case VybzTabVC
+}
