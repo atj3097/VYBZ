af63602f83d934e1c88e803c1ca7e3c691f7a4bd
Adam <adamjackson@pursuit.org>
2019-12-13 16:13:18 -0500

    Added Piano VC, Testing Segues to Piano from Moods Collection View

--- Vybz/VCs/DVCs/ScaleVC.swift
+++ Vybz/VCs/DVCs/ScaleVC.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
-
-class ScaleVC: UIViewController {
-
+import MusicTheorySwift
+import GLNPianoView
+class ScaleVC: UIViewController, GLNPianoViewDelegate {
+    private let audioEngine = AudioEngine()
+    
+    @IBOutlet weak var keyboard: GLNPianoView!
+    
+    @IBOutlet weak var fascia: UIView!
+    
+    
+    
+    
+    @IBAction func showNotes(_ sender: UISwitch) {
+        keyboard.toggleShowNotes()
+    }
+    
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+         let layer = CAGradientLayer()
+               layer.frame = fascia.bounds
+               layer.colors = [UIColor.black.cgColor, UIColor.darkGray.cgColor, UIColor.black.cgColor]
+               layer.startPoint = CGPoint(x: 0.0, y: 0.80)
+               layer.endPoint = CGPoint(x: 0.0, y: 1.0)
+               fascia.layer.insertSublayer(layer, at: 0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        keyboard.delegate = self
+         audioEngine.start()
         // Do any additional setup after loading the view.
     }
     
@@ -26,5 +52,33 @@ class ScaleVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    func pianoKeyDown(_ keyNumber: Int) {
+        audioEngine.sampler.startNote(UInt8(keyboard.octave + keyNumber), withVelocity: 64, onChannel: 0)
+       
+    }
+
+    func pianoKeyUp(_ keyNumber: Int) {
+        audioEngine.sampler.stopNote(UInt8(keyboard.octave + keyNumber), onChannel: 0)
+    }
+    func autoHighlight(score: [[String]], position: Int, loop: Bool, tempo: Double, play: Bool) {
+        keyboard.highlightKeys(noteNames: score[position], color: UIColor.init(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.35), play: play)
+        let delay = 120.0/tempo
+        let nextPosition = position + 1
+        if nextPosition < score.count {
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+                self?.autoHighlight(score: score, position: nextPosition, loop: loop, tempo: tempo, play: play)
+            }
+        } else {
+            if loop {
+                DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+                    self?.autoHighlight(score: score, position: 0, loop: loop, tempo: tempo, play: play)
+                }
+            } else {
+                DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+                    self?.keyboard.reset(didPlay: play)
+                }
+            }
+        }
+    }
 
 }
