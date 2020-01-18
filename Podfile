ea9a277db6b28e62e3e1c1d3adef691415c6614f
Adam <adamjackson@pursuit.org>
2020-01-18 17:57:40 -0500

    Added styled textfield and gradient button

--- Podfile
+++ Podfile
@@ -12,7 +12,8 @@ target 'Vybz' do
 	pod 'Firebase/Firestore' 
 	pod 'Firebase/Storage'  
 	pod 'AnimatedCollectionViewLayout' 
-	pod 'YouTubePlayer-Swift', '~> 1.0'
+	pod 'YouTubePlayer-Swift', '~> 1.0' 
+	pod 'TextFieldEffects'
   target 'VybzTests' do
     inherit! :search_paths
     # Pods for testing
--- Vybz/VCs/Authentication/LoginVC.swift
+++ Vybz/VCs/Authentication/LoginVC.swift
@@ -10,13 +10,15 @@ import UIKit
 import Firebase
 import AVFoundation
 import AVKit
+import TextFieldEffects
 class LoginVC: UIViewController {
     private var animator: UIViewPropertyAnimator!
   var avPlayer: AVPlayer!
     var avPlayerLayer: AVPlayerLayer!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var emailTextField: YoshikoTextField!
     @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var passwordTextField: YoshikoTextField!
     @IBOutlet weak var vybzLogo: UIImageView!
     
     @IBAction func signIn(_ sender: UIButton) {
@@ -40,6 +42,7 @@ class LoginVC: UIViewController {
                    self.handleLoginResponse(with: result)
                }
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         emailTextField.delegate = self
@@ -47,10 +50,11 @@ class LoginVC: UIViewController {
         playSplashVid()
         animateLogo()
         signInButton.roundButton(button: signInButton)
+        signInButton.setGradientBackground()
     }
     
     func playSplashVid() {
-        let theURL = URL(string: "https://firebasestorage.googleapis.com/v0/b/vybz-f8bbc.appspot.com/o/metro-boomin-presents-good-cook-up-vol-1.mp4?alt=media&token=09726861-b1af-4b64-b308-d31b0675c33a")
+        let theURL = URL(string: "https://firebasestorage.googleapis.com/v0/b/test-1a5af.appspot.com/o/metro-boomin-presents-good-cook-up-vol-1.mp4?alt=media&token=f275f69b-c5aa-4731-8c58-d71b2e84b791")
         avPlayer = AVPlayer(url: theURL!)
         avPlayerLayer = AVPlayerLayer(player: avPlayer)
         avPlayerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
@@ -101,4 +105,43 @@ extension LoginVC: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
     }
+}
+extension UITextField {
+  func setGradient() {
+            let gradient = CAGradientLayer()
+            gradient.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height)
+            gradient.startPoint = CGPoint(x:0.0, y:0.5)
+            gradient.endPoint = CGPoint(x:1.0, y:0.5)
+            gradient.colors = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor]
+    self.layer.addSublayer(gradient)
+  }
+
+}
+extension UIView {
+    
+    func setGradientBackground() {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
+struct Colors {
+    
+    static let brightOrange     = UIColor(red: 255.0/255.0, green: 69.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static let red              = UIColor(red: 255.0/255.0, green: 115.0/255.0, blue: 115.0/255.0, alpha: 1.0)
+    static let orange           = UIColor(red: 255.0/255.0, green: 175.0/255.0, blue: 72.0/255.0, alpha: 1.0)
+    static let blue             = UIColor(red: 74.0/255.0, green: 144.0/255.0, blue: 228.0/255.0, alpha: 1.0)
+    static let green            = UIColor(red: 91.0/255.0, green: 197.0/255.0, blue: 159.0/255.0, alpha: 1.0)
+    static let darkGrey         = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1.0)
+    static let veryDarkGrey     = UIColor(red: 13.0/255.0, green: 13.0/255.0, blue: 13.0/255.0, alpha: 1.0)
+    static let lightGrey        = UIColor(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+    static let black            = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static let white            = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
 }
